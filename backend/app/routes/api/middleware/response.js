12751116722bc96e52b542bbe0e545cb2b1e2e79
Abhishek Sriram <68271853+Noobsiecoder@users.noreply.github.com>
2021-03-26 16:49:38 +0530

    Revert "completed User Auth using MERN stack"
    This reverts commit 55a123e1f73276761aa2f7ae9b0f27a58fcb0c94.

--- backend/app/routes/api/middleware/response.js
+++
@@ -1,9 +0,0 @@
-// Function to give response with { httpStatusCode }, { route } and {response}
-const expressResponse = (res, httpStatusCode, route, response) => {
-  res.status(httpStatusCode).json({
-    message: `POST request handled for /${route}`,
-    response: response,
-  });
-};
-
-module.exports = expressResponse;
